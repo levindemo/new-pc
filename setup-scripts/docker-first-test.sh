b365b2746fbc529bd96631c29d05688fcf6e15af
gaozheng@joygames.cc <gaozheng@joygames.cc>
2025-06-29 00:12:49 +0800

    enhance

--- setup-scripts/docker-first-test.sh
+++ setup-scripts/docker-first-test.sh
@@ -6,7 +6,8 @@ docker login
 docker pull nginx
 
 
-docker run --name my-nginx -p 8080:80 -d nginx
+docker rm -f test-nginx || echo "skip as no history container"
+docker run --name test-nginx -p 8080:80 -d nginx
 
 
 curl http://localhost:8080
