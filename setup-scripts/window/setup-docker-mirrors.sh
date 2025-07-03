f94ec5e469a4ce387e3c572953155e6131ca6cbe
gaozheng@joygames.cc <gaozheng@joygames.cc>
2025-07-04 00:17:02 +0800

    add docker setup

--- setup-scripts/window/setup-docker-mirrors.sh
+++ setup-scripts/window/setup-docker-mirrors.sh
@@ -2,7 +2,7 @@
 
 user=`whoami`
 
-/c/Users/${user}/.docker/daemon.json /c/Users/${user}/.docker/.daemon.json_backup_`date +"%Y%m%d_%H%M%S"`
+cp /c/Users/${user}/.docker/daemon.json /c/Users/${user}/.docker/daemon.json /c/Users/${user}/.docker/.daemon.json_backup_`date +"%Y%m%d_%H%M%S"`
 cp docker.daemon.json /c/Users/${user}/.docker/daemon.json
 
-cat /c/Users/${user}/AppData/Roaming/Code/User/settings.json
+cat /c/Users/${user}/.docker/daemon.json
